c99835e155090525886bad8cc12561da1d48218f
mansur serbest <mansur.serbest@outlook.com>
2023-08-14 16:13:23 +0200

    setup base for turn based games and rock papers scissors game type

---
+++ Source/Olme/Public/Base/TurnBased/HUDTurnBased.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "HUDTurnBased.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OLME_API AHUDTurnBased : public AHUD
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Olme/Public/Base/TurnBased/PlayerControllerTurnBased.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerControllerTurnBased.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OLME_API APlayerControllerTurnBased : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
